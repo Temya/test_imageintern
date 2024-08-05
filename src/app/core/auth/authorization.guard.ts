f5d0a683469eefa9e904d8782d67c37c36bd39f5
unknown <arti_sad@mail.ru>
2024-08-05 21:52:19 +0300

    Edit authGuard

--- src/app/core/auth/authorization.guard.ts
+++ src/app/core/auth/authorization.guard.ts
@@ -4,16 +4,13 @@ import { AuthService } from "./auth.service";
 
 export const authorizationGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
-  const router = inject(Router);
-  const redirectIfAuth = route.data?.["redirectIfAuth"];
-  if (redirectIfAuth && authService.isAuth) {
-    router.navigate([" "]);
-    return false;
-  } else if (redirectIfAuth) return true;
-  if (!authService.isAuth && !redirectIfAuth) {
-    router.navigate(["/auth"]);
-    return false;
-  }
+	const router = inject(Router);
+	const redirectIfAuth = route.data?.["redirectIfAuth"];
+	if (redirectIfAuth && authService.isAuth) router.navigate(["/major/menu"]);
+	if (!authService.isAuth) {
+		router.navigateByUrl("/major");
+		return false;
+	}
 
-  return authService.isAuth;
+	return authService.isAuth;
 };
