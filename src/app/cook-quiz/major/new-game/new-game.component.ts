239342ae77714b72d8541ca58b6e50e63092b589
Росса Артём Дмитриевич <artsiom.rossa@softclub.by>
2024-08-19 16:04:55 +0300

    Add options

--- src/app/cook-quiz/major/new-game/new-game.component.ts
+++ src/app/cook-quiz/major/new-game/new-game.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, Inject, inject } from "@angular/core";
 import {
   FormArray,
   FormBuilder,
@@ -16,9 +16,11 @@ import {
 } from "@taiga-ui/kit";
 import { QuestionsHard, QuestionsLite, QuestionsMedium } from "../../../core/questions/questions.data";
 import { Answer, Questions, Round } from "../../../interfaces/questions";
-import { TuiButtonModule } from "@taiga-ui/core";
+import { TuiButtonModule, TuiDialogContext, TuiDialogModule, TuiDialogService, TuiDialogSize } from "@taiga-ui/core";
 import { QuizSettingsService } from "../../shared/utils/quiz-settings.service";
-import { Router, RouterLink } from "@angular/router";
+import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus';
+import { RouterLink } from "@angular/router";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: "app-new-game",
@@ -31,7 +33,8 @@ import { Router, RouterLink } from "@angular/router";
     TuiTilesModule,
     TuiButtonModule,
     TuiInputModule,
-    RouterLink
+    RouterLink,
+    TuiDialogModule
   ],
   templateUrl: "./new-game.component.html",
   styleUrl: "./new-game.component.scss",
@@ -43,19 +46,20 @@ export class NewGameComponent {
   public round = 1;
   public answers: Answer[] = [];
   public complexity: Round[] = [];
+  public result = 0;
+
+  private readonly destroy = inject(DestroyRef);
 
   constructor(
     private readonly fb: FormBuilder, 
     private readonly cdr: ChangeDetectorRef, 
-    private readonly quizService: QuizSettingsService) {
+    private readonly quizService: QuizSettingsService,
+    @Inject(TuiDialogService) private readonly dialogs: TuiDialogService) {
     this.quizService.getGameMusic();
 
     if(this.quizService.getSettings().complexity === "easy") {this.complexity = QuestionsLite}
     if(this.quizService.getSettings().complexity === "medium") {this.complexity = QuestionsMedium}
     if(this.quizService.getSettings().complexity === "hard") {this.complexity = QuestionsHard}
-    if(this.round === 2) {
-      this.answers = this.randomPosition(QuestionsLite[1].questions[0].answers);
-    }
 
     this.generateForm();
   }
@@ -94,29 +98,26 @@ export class NewGameComponent {
     )?.controls[index2] as FormControl;
   }
 
-  public get questions(): Questions[] {
-    console.log(this.complexity[this.round - 1].questions);
-    
+  public get questions(): Questions[] {    
     return this.complexity[this.round - 1].questions;
-  }
-
-  public checkAnswers(): void {
-    this.comparisonOfResults();
-    // this.round = 2;
-    // this.cdr.detectChanges();
   }
 
   public comparisonOfResults(): void {
       this.complexity[this.round - 1].questions.forEach((question, index) => {
         if (question.type === "radio") {
-          console.log((this.getFormControl(index).value as Answer).right);
+          if((this.getFormControl(index).value as Answer).right) {
+            this.result = this.result + 10
+          }
         }
         if (question.type === "input") {
-          console.log(this.getFormControl(index).value?.toLowerCase(), question.answers[0].text?.toLowerCase());
-          console.log(this.getFormControl(index).value?.toLowerCase() === question.answers[0].text?.toLowerCase());
+          if(this.getFormControl(index).value?.toLowerCase() === question.answers[0].text?.toLowerCase()) {
+            this.result = this.result + 20
+          }
         }
         if (question.type === "drag") {
-          this.checkDragAndDrop(this.answers);
+          this.checkDragAndDrop(this.answers).forEach((data) => {
+            data === "good" ? this.result = this.result + 10 : null;
+          });
         }
         if (question.type === "check") {
           ((this.form.controls["questions"] as FormArray).controls[index] as FormArray).controls.forEach((item, i) => {
@@ -128,21 +129,49 @@ export class NewGameComponent {
       })
       if(this.round !== 2) {
         this.round++;
+        this.answers = this.randomPosition(QuestionsLite[1].questions[0].answers);
         this.generateForm();
+      } else {
+        
       }
   }
 
-  public checkDragAndDrop(drag: Answer[]): void {
+  public checkDragAndDrop(drag: Answer[]): string[] {
+    let array: string[] = [];
     this.questions[0].images?.forEach((image, id) => {
       drag.forEach((data, index) => {
         if(image.text === data.text) {
-          this.order.get(index) === id ? console.log("good") : console.log("bad")
+          this.order.get(index) === id ? array.push("good") : array.push("bad")
         }
       })
     })
+    return array
   }
 
   public randomPosition(array: Answer[]): Answer[] {
     return array.sort(() => Math.random() - 0.5);
+  }
+
+  public onClick(
+    content: PolymorpheusContent<TuiDialogContext>,
+    header: PolymorpheusContent,
+    size: TuiDialogSize,
+  ): void {
+    this.comparisonOfResults();
+    this.dialogs
+    .open(content, {
+      label: `Молодец, твой результат: ${this.result}`,
+      header,
+      size,
+      dismissible: false,
+      closeable: false
+    })
+    .pipe(takeUntilDestroyed(this.destroy))
+    .subscribe();
+  }
+
+  public restartGame(): void {
+    this.round = 1;
+    this.generateForm();
   }
 }
